ce8ae94cbe222e2c9f8ea7093509fd98b75b350a
Usuário Convidado <Guest@pa06micro09.local>
2017-08-28 20:26:48 -0300

    First commit

--- DesenvAula01/ViewController.swift
+++ DesenvAula01/ViewController.swift
@@ -10,15 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tfNumber: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        tfNumber.resignFirstResponder()
+        view.endEditing(true)
     }
+   
 
 
 }
